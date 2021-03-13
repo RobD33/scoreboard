e2faff90e427187e5618abb208a4b91aaf745d21
Rob Davidson <r.a.davidson@live.co.uk>
2021-03-13 14:58:06 +0000

    create login page and auth request

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -2,11 +2,11 @@ import axios from 'axios';
 import configVars from '../config'
 
 const authorize = (username:String, password: String) => {
-  axios.post(`${configVars.servicePath}/authorize`, { username, password})
-  .then(response => response.data.token )
-  .catch(console.log)
+  return axios.post(`${configVars.servicePath}/authorize`, { username, password})
+    .then(response => response.data.token )
+    .catch(console.log)
 }
 
 const api = { authorize }
 
-export default api
+export default api;
